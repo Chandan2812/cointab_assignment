111a01c1abaf86e246a945dd7acb1b7157c54c7b
Chandan2812 <chandanpms@gmail.com>
2024-02-15 10:14:17 +0530

    page 1 completed

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require("cors")
 const {Connection} = require("./config/db");
+const {userRouter} = require("./routes/user.route")
+const {postRouter} = require("./routes/post.route")
 require("dotenv").config()
 
 const PORT=process.env.PORT || 8000
@@ -7,8 +10,10 @@ const PORT=process.env.PORT || 8000
 const app=express()
 
 app.use(express.json())
+app.use(cors());
 
-
+app.use('/users', userRouter);
+app.use('/posts', postRouter);
 
 app.listen(PORT,async()=>{
     try {
